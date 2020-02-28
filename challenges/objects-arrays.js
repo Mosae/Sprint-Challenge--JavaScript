66f5725120eb95aa88869c98f32fd30b96e3890b
Mosae Litsoane <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-02-28 10:44:11 -0600

    Finished lowerCaseAnimalNames

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -243,6 +243,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
+zooAnimals.map(function (a) {
+  lowCaseAnimalNames.push(a.animal_name.toLowerCase())
+})
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
